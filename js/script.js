4ef99947e316aaaf7c81c17d90700a84f6b6c701
Duda Silveira <dudasouzas007@gmail.com>
2025-08-07 17:38:15 -0300

    alterei o max- width de 48%, js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const faixa = document.querySelector('.faixa-carrossel');
-    const itens = document.querySelectorAll('.item-carrossel');
-    const larguraItem = itens[0].offsetWidth;
-    let contadorClones = 0;
+<script>
+      
+    const container = document.getElementById('carrossel');
+    const content = document.getElementById('heroImagens');
 
-    //clone
-    itens.forEach(item => {
-        const clone = item.cloneNode(true);
-        faixa.appendChild(clone);
-        contadorClones++;
-    });
+    const clone= content.cloneNode(true);
+    container.appendChild(clone);
+
+     let scrollY = 0;
+     const velocidade = 0.7;
+
+    function scrollInfinito() {
+    scrollY += velocidade;
+    container.scrollTop = scrollY;
     
-    // velocidadee
-    const duracaoBase = 30;
-    faixa.style.animationDuration = ${duracaoBase * (contadorClones / itens.length)}s;
-    
-    // impede o reset por 1 loop
-    const duracaoOriginal = faixa.style.animationDuration;
+    if (scrollY >= content.scrollHeight) {
+      scrollY = 0;
+    }
 
-    // reiniciar a animação no final para evitar um salto
-    faixa.addEventListener('animationiteration', () => {
-        faixa.style.animation = 'none';
-        faixa.offsetHeight; // trigger reflow / nao redimensionar a pagina para evitar um refresh
-        faixa.style.animation = null;
-        faixa.style.animationDuration = duracaoOriginal; // resetar para a duração original
-    });
+    requestAnimationFrame(scrollInfinito);
+  }
 
-    // pausar a animação como hover
-    faixa.addEventListener('mouseover', () => {
-        faixa.style.animationPlayState = 'paused';
-    });
-
-    // retomar a animação pós hover
-    faixa.addEventListener('mouseout', () => {
-        faixa.style.animationPlayState = 'running';
-    });
-});
+  scrollInfinito();
+</script>
